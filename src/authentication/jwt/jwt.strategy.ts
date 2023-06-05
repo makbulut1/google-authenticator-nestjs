d7df7e6f50b690ee9673a51958ff24d731f225fc
makbulut1 <makbulut@student.42istanbul.com.tr>
2023-06-05 23:20:02 +0300

    [change]: entities name

--- src/authentication/jwt/jwt.strategy.ts
+++ src/authentication/jwt/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../../users/users.service';
-import { TokenPayload } from '../etities/token-payload.entity';
+import { TokenPayload } from '../entities/token-payload.entity';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
